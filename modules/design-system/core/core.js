3b70aab3d17c23351c0f02fb389922924e662941
Bartek <cierniabartek0802@gmail.com>
2023-01-07 23:25:54 +0100

    added functions to create H1 and paragraph

--- modules/design-system/core/core.js
+++ modules/design-system/core/core.js
@@ -16,6 +16,24 @@ export const createImg = (src, alt, additionalClasses) => {
     return img;
 };
 
+export const createH1 = (text, additionalClasses = []) => {
+    const h1= document.createElement('h1');
+
+    h1.innerText = text;
+    h1.classList.add(...additionalClasses);
+
+    return h1;
+};
+
+export const createParagraph = (text, additionalClasses) => {
+    const p = document.createElement('p');
+
+    p.textContent = text;
+    p.classList.add(...additionalClasses);
+
+    return p;
+}
+
 export const createBtn = (text, additionalClasses, onClickHandler) => {
     const btn = document.createElement('button');
 
