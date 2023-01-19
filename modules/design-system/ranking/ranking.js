7cce0009e5735091d9bcfab332ba4dd9949bf0a7
Bartek <cierniabartek0802@gmail.com>
2023-01-19 22:37:18 +0100

    added clear ranking logic

--- modules/design-system/ranking/ranking.js
+++ modules/design-system/ranking/ranking.js
@@ -3,6 +3,7 @@ import {addRowToTable} from "../core/core.js";
 const rankingTransitionBtn = document.querySelector('.btn-ranking-transition');
 const ranking = document.querySelector('.ranking');
 const rankingTable = document.querySelector('.ranking-table-body');
+const clearRankingBtn = ranking.querySelector('.btn-clear-ranking');
 
 const rankingFromLocalStorage = localStorage.getItem('ranking');
 let resultRanking = [];
@@ -10,6 +11,12 @@ let resultRanking = [];
 if(rankingFromLocalStorage!==null){
     resultRanking = JSON.parse(rankingFromLocalStorage);
 }
+
+clearRankingBtn.addEventListener('click', () => {
+    localStorage.removeItem('ranking');
+    resultRanking = [];
+    addRowToTable(rankingTable,resultRanking);
+})
 
 rankingTransitionBtn.addEventListener('click', () => {
     if(ranking.classList.contains('ranking-hidden')){
@@ -24,9 +31,7 @@ export const addNewResultToRankingArray = (playersName, time) => {
         Name: playersName,
         Time: time,
     }
-
     resultRanking.push(newResult);
-
     localStorage.setItem("ranking", JSON.stringify(resultRanking));
 }
 
